fdf374b97a4d23f08365717deb5566e09a365dd6
Kevin Bell <bellKevin@pm.me>
2022-10-13 21:39:46 -0600

    Update ch7bankAccounts.cpp

--- ch7bankAccounts/ch7bankAccounts/ch7bankAccounts.cpp
+++ ch7bankAccounts/ch7bankAccounts/ch7bankAccounts.cpp
@@ -2,19 +2,92 @@
 //
 
 #include <iostream>
-
+using namespace std;
+struct BankAccount
+{
+    string account = {};
+    int accountNumber{};
+    double balance{};
+};
+//function prototypes
+void withdraw(BankAccount& account, double amount);
+void deposit(BankAccount& account, double amount);
+void displayAccount(BankAccount account);
+void withdraw2(BankAccount& account2, double amount2);
+void deposit2(BankAccount& account2, double amount2);
+void displayAccount2(BankAccount account2);
+double amount, amount2;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Ch 7 Bank Accounts by Kevin Bell\n\n";
+    BankAccount account = {"Checking", 1234, 100}, account2 = {"Savings", 9876, 50};
+    displayAccount(account);
+    withdraw(account, amount);
+    cout << endl;
+    displayAccount(account);
+    deposit(account, amount);
+    cout << endl;
+    displayAccount(account);
+    cout << endl;
+    displayAccount2(account2);
+    deposit2(account2, amount2);
+    cout << endl;
+    displayAccount2(account2);
+    withdraw2(account2, amount2);
+    cout << endl;
+    displayAccount2(account2);
+    system("pause");
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void withdraw(BankAccount& account, double amount)
+{
+    cout << "How much would you like to withdraw? ";
+    cin >> amount;
+    if (amount > account.balance)
+    {
+        cout << "Insufficient funds. Transaction cancelled.\n";
+    }
+    else
+    {
+        account.balance -= amount;
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void deposit(BankAccount& account, double amount)
+{
+    cout << "How much would you like to deposit? ";
+    cin >> amount;
+    account.balance += amount;
+}
+
+void displayAccount(BankAccount account)
+{
+    cout << account.account << " " << account.accountNumber << " " << "has a balance of $"<< account.balance << endl;
+}
+
+void withdraw2(BankAccount& account2, double amount2)
+{
+    cout << "How much would you like to withdraw? ";
+    cin >> amount2;
+    if (amount2 > account2.balance)
+    {
+        cout << "Insufficient funds. Transaction cancelled.\n";
+    }
+    else
+    {
+        account2.balance -= amount2;
+    }
+}
+
+void deposit2(BankAccount& account2, double amount2)
+{
+    cout << "How much would you like to deposit? ";
+    cin >> amount2;
+    account2.balance += amount2;
+}
+
+void displayAccount2(BankAccount account2)
+{
+    cout << account2.account << " " << account2.accountNumber << " " << "has a balance of $"<< account2.balance << endl;
+}
