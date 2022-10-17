3a17a659a4b8f4f94a909eedd6a4143e2cd5d8eb
Kevin Bell <bellKevin@pm.me>
2022-10-17 11:12:46 -0600

    Update ch7bankAccounts.cpp

--- ch7bankAccounts/ch7bankAccounts/ch7bankAccounts.cpp
+++ ch7bankAccounts/ch7bankAccounts/ch7bankAccounts.cpp
@@ -3,91 +3,71 @@
 
 #include <iostream>
 using namespace std;
+
 struct BankAccount
 {
     string account = {};
     int accountNumber{};
     double balance{};
 };
+
 //function prototypes
-void withdraw(BankAccount& account, double amount);
-void deposit(BankAccount& account, double amount);
+double withdraw(BankAccount& account, double amount);
+double deposit(BankAccount& account, double amount);
 void displayAccount(BankAccount account);
-void withdraw2(BankAccount& account2, double amount2);
-void deposit2(BankAccount& account2, double amount2);
-void displayAccount2(BankAccount account2);
-double amount, amount2;
+double amount;
+
 int main()
 {
     cout << "Ch 7 Bank Accounts by Kevin Bell\n\n";
-    BankAccount account = {"Checking", 1234, 100}, account2 = {"Savings", 9876, 50};
+    BankAccount account = { "Checking", 1234, 100 }, account2 = { "Savings", 9876, 50 };
     displayAccount(account);
+    cout << "How much would you like to withdraw? ";
+    cin >> amount;
     withdraw(account, amount);
     cout << endl;
     displayAccount(account);
+    cout << "How much would you like to deposit? ";
+    cin >> amount;
     deposit(account, amount);
     cout << endl;
     displayAccount(account);
     cout << endl;
-    displayAccount2(account2);
-    deposit2(account2, amount2);
-    cout << endl;
-    displayAccount2(account2);
-    withdraw2(account2, amount2);
-    cout << endl;
-    displayAccount2(account2);
+	displayAccount(account2);
+	cout << "How much would you like to deposit? ";
+	cin >> amount;
+	deposit(account2, amount);
+	cout << endl;
+	displayAccount(account2);
+	cout << "How much would you like to withdraw? ";
+	cin >> amount;
+	withdraw(account2, amount);
+	cout << endl;
+	displayAccount(account2);
     system("pause");
     return 0;
-}
+} //end main
 
-void withdraw(BankAccount& account, double amount)
+double withdraw(BankAccount& account, double amount)
 {
-    cout << "How much would you like to withdraw? ";
-    cin >> amount;
-    if (amount > account.balance)
+    if (amount > account.balance) // check for sufficient funds
     {
         cout << "Insufficient funds. Transaction cancelled.\n";
     }
     else
     {
         account.balance -= amount;
-    }
-}
+	} //end if
+	return amount;
+} //end withdraw
 
-void deposit(BankAccount& account, double amount)
-{
-    cout << "How much would you like to deposit? ";
-    cin >> amount;
+double deposit(BankAccount& account, double amount)
+{ 
     account.balance += amount;
-}
+	return amount;
+} //end deposit
 
 void displayAccount(BankAccount account)
 {
-    cout << account.account << " " << account.accountNumber << " " << "has a balance of $"<< account.balance << endl;
-}
-
-void withdraw2(BankAccount& account2, double amount2)
-{
-    cout << "How much would you like to withdraw? ";
-    cin >> amount2;
-    if (amount2 > account2.balance)
-    {
-        cout << "Insufficient funds. Transaction cancelled.\n";
-    }
-    else
-    {
-        account2.balance -= amount2;
-    }
-}
-
-void deposit2(BankAccount& account2, double amount2)
-{
-    cout << "How much would you like to deposit? ";
-    cin >> amount2;
-    account2.balance += amount2;
-}
-
-void displayAccount2(BankAccount account2)
-{
-    cout << account2.account << " " << account2.accountNumber << " " << "has a balance of $"<< account2.balance << endl;
-}
+    cout << account.account << " " << account.accountNumber << " " << "has a balance of $" << account.balance << endl;
+} //end displayAccount
